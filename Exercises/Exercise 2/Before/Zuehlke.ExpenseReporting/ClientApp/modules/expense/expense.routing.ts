1cbbb9037c0baf65267194a5e9eda0efc4981286
mre <mre@FRAN0565>
2016-11-11 13:21:14 +0100

    Added Before solution to Exercise 2

---
+++ Exercises/Exercise 2/Before/Zuehlke.ExpenseReporting/ClientApp/modules/expense/expense.routing.ts
@@ -0,0 +1,10 @@
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
+
+import { ExpenseOverviewComponent } from './components/expense-overview.component';
+
+export const expenseRoutes: Routes = [
+    { path: 'overview', component: ExpenseOverviewComponent }
+];
+
+export const expenseRouting: ModuleWithProviders = RouterModule.forChild(expenseRoutes);
