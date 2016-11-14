ca65cfbf5a519788fff9fddd356d8b3ca38e3a45
stanbe <stefan.benz@zuehlke.com>
2016-11-14 16:53:25 +0100

    add FormsModule to before 2 solution

--- Exercises/Exercise 2/Before/Zuehlke.ExpenseReporting/ClientApp/modules/expense/expense.module.ts
+++ Exercises/Exercise 2/Before/Zuehlke.ExpenseReporting/ClientApp/modules/expense/expense.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { expenseRouting } from './expense.routing';
 import { ExpenseOverviewComponent } from './components/expense-overview.component';
 
 @NgModule({
   imports: [
-      CommonModule, expenseRouting
+      CommonModule, FormsModule, expenseRouting
   ],
   declarations: [
     ExpenseOverviewComponent,
