156928a4c08ec8d62bd72d8c36d4356ad237c960
stanbe <stefan.benz@zuehlke.com>
2016-11-15 11:19:17 +0100

    add fancier solution to after 4 solution

--- Exercises/Exercise 4/After/Zuehlke.ExpenseReporting/ClientApp/modules/expense/components/expense-form.component.ts
+++ Exercises/Exercise 4/After/Zuehlke.ExpenseReporting/ClientApp/modules/expense/components/expense-form.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
-import { Expense } from '../model/expense';
+import { Expense, Reason } from '../model/expense';
 import { ExpenseService } from '../services/expense.service';
 
 @Component({
@@ -13,4 +13,17 @@ export class ExpenseFormComponent {
 
     @Input() expense: Expense;
 
+
+    get reasons() {
+        let theReasons = [];
+        for (var property in Reason) {
+            if (Reason.hasOwnProperty(property) && !isNaN(parseInt(property))) {
+                theReasons.push(Reason[property]);
+            }
+        }
+
+        return theReasons;
+    }
 }
+
+
