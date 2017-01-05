503116423e206a5ecd744f85f14c1c7f4dbc44dc
Markus Rehrs <markus.rehrs@zuehlke.com>
2017-01-05 15:18:27 +0100

    Merged changes from Exercise 8 / Before zo Exercise 7 / After

--- Exercises/Exercise 7/After/Zuehlke.ExpenseReporting/ClientApp/modules/app/app.module.ts
+++ Exercises/Exercise 7/After/Zuehlke.ExpenseReporting/ClientApp/modules/app/app.module.ts
@@ -4,14 +4,21 @@ import { UniversalModule } from 'angular2-universal';
 import { AppComponent } from './components/app.component'
 import { NavMenuComponent } from './components/navmenu.component';
 import { HomeComponent } from './components/home.component';
+
 import { ExpenseModule } from '../expense/expense.module';
 
 @NgModule({
-    bootstrap: [ AppComponent ],
-    declarations: [AppComponent, NavMenuComponent, HomeComponent],
+    bootstrap: [
+        AppComponent
+    ],
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        HomeComponent
+    ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
-        ExpenseModule, 
+        ExpenseModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
@@ -19,4 +26,5 @@ import { ExpenseModule } from '../expense/expense.module';
         ])
     ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- Exercises/Exercise 7/After/Zuehlke.ExpenseReporting/ClientApp/modules/expense/components/expense-overview.component.spec.ts
+++ Exercises/Exercise 7/After/Zuehlke.ExpenseReporting/ClientApp/modules/expense/components/expense-overview.component.spec.ts
@@ -9,15 +9,15 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { ExpenseOverviewComponent } from './expense-overview.component';
 import { ExpenseFilterPipe } from '../pipes/expense-filter.pipe';
 import { ExpenseService } from '../services/expense.service';
-import { Expense, Reason } from '../model/expense';
+import { ExpenseRecord, ExpenseReason } from '../model/expense';
 
 describe('ExpenseOverviewComponent', () => {
 
     let expenseOverviewComponent: ExpenseOverviewComponent;
     let fixture: ComponentFixture<ExpenseOverviewComponent>;
 
-    const expense1 = new Expense('1', 'Anakin Skywalker', Reason.Bus, new Date('2016/1/12'), 12.22, 'Visiting Mom');
-    const expense2 = new Expense('2', 'Yoda', Reason.Flight, new Date('2016/5/12'), 10, 'Flight to Coruscant');
+    const expense1 = new ExpenseRecord('1', 'Anakin Skywalker', ExpenseReason.Bus, new Date('2016/1/12'), 12.22, 'Visiting Mom');
+    const expense2 = new ExpenseRecord('2', 'Yoda', ExpenseReason.Flight, new Date('2016/5/12'), 10, 'Flight to Coruscant');
     const testExpenses = [expense1, expense2];
 
     let expenseService: ExpenseService;
@@ -46,10 +46,10 @@ describe('ExpenseOverviewComponent', () => {
     it('should show two expenses after OnInit', async(() => { //use async to handle getExpenses request
         fixture.detectChanges();
         fixture.whenStable().then(() => { // wait for async getExpenses
-            fixture.detectChanges();        // update view with expsenses
+            fixture.detectChanges(); // update view with expsenses
 
             expect(expenseService.getExpenses).toHaveBeenCalledTimes(1); //check that getExpenses() from the expense service was called
-           
+
             const tableBody = fixture.debugElement.query(By.css('tbody')); //check for table to contain the two expenses
             expect(tableBody.nativeElement.children.length).toEqual(2);
             expect(tableBody.nativeElement.children[0].children[0].textContent).toContain('ANAKIN SKYWALKER');
@@ -77,5 +77,3 @@ describe('ExpenseOverviewComponent', () => {
     }));
 
 });
-
-
