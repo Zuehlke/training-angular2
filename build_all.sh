7013bff6a0161ec4c908f522b2981484d647525f
Jens Bertram <jens.bertram@zuehlke.com>
2017-10-06 11:43:21 +0200

    improved build all script

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,29 @@
 #!/bin/sh
 
 for f in $(find . -name '*Zuehlke.ExpenseReporting.csproj'); do
-
-	cd $(dirname $(dirname $f));
 	
+	# build frontend
+	directory=$(dirname $f)
+	cd $directory;
+	
+	if [ -f webpack.config.js ]; then
+		echo "Building frontend in $directory";
+		pwd
+		
+		npm install;
+
+		echo "webpack vendor.js"
+		webpack --config webpack.config.vendor.js;
+
+		echo "webpack ...";
+		webpack;
+	fi
+
+	cd -;
+
+	# build dotnet project
+	cd $(dirname $(dirname $f));
+
 	echo "dotnet restore ..."
 	dotnet restore;
 	echo "dotnet build ..."
@@ -13,18 +33,3 @@ for f in $(find . -name '*Zuehlke.ExpenseReporting.csproj'); do
 	cd - > /dev/null;
 
 done
-
-: "
-for f in $(find . -name '*Zuehlke.ExpenseReporting.Test.csproj'); do
-	echo "Migrate $f";
-
-	sed -i '' -e 's/netcoreapp1\.1/netcoreapp2\.0/g' $f;
-done
-
-for f in $(find . -name '*global.json'); do
-	echo "Migrate $f";
-
-	sed -i '' -e 's/\"version\": \"1\.1\.1\"/\"version\": \"2\.0\.0\"/g' $f;
-done
-
-"
