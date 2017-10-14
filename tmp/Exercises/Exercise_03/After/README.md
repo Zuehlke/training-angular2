37d59c1c00f60cf2c8cb08d4ff076eaea897987b
Jens Bertram <jens.bertram@zuehlke.com>
2017-10-14 13:29:06 +0200

    migrated exercise3

---
+++ tmp/Exercises/Exercise_03/After/README.md
@@ -0,0 +1,3 @@
+## Exercise 3: Result ##
+
+This folder contains the application as it is after you have completed this exercise.
---
+++ tmp/Exercises/Exercise_03/After/Zuehlke.ExpenseReporting/ClientApp/modules/expense/services/expense.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+import { ExpenseRecord } from '../model/expense';
+
+@Injectable()
+export class ExpenseService {
+
+    private expenseUrl = 'api/expenses';
+
+    constructor(private http: Http) {}
+
+    getExpenses(): Promise<ExpenseRecord[]> {
+        return this.http.get(this.expenseUrl)
+                        .map(response => response.json() || [])
+                        .toPromise();
+    }
+
+    deleteExpense(expense: ExpenseRecord): Promise<Response> {
+        return this.http.delete(`${this.expenseUrl}/${expense.id}`)
+                        .toPromise();
+    }
+}
